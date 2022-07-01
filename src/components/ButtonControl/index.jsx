366c4d34d557d4008a52503e4753adf258f31410
Peritract <dan@getfutureproof.co.uk>
2022-07-01 09:49:16 +0100

    Add coverage command

--- src/components/ButtonControl/index.jsx
+++ src/components/ButtonControl/index.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 const ButtonControl = ({ displayText, clickHandler }) => {
 
-    const relc = () => {
-        clickHandler(3)
-    }
-
-    return <button onClick={ relc }>{ displayText }</button>
+    return <button onClick={ clickHandler }>{ displayText }</button>
 }
 
 export default ButtonControl;
